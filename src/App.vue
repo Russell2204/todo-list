2ed90aa1e13d2425130036eb25ab6227afd2cd29
Russell2204 <ruslanabdjemilov@gmail.com>
2023-08-21 15:31:42 +0500

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Navbar @searchValue="search = $event" />
+  <Modal @showOrHideModal="showOrHideModal" v-show="showModal" />
+  <a href="#" class="addNote" @click.prevent="showModal = true">
+    <img src="@/assets/img/edit.svg" alt="">
+  </a>
+  
+</template>
+<script>
+import Modal from './components/Modal.vue'
+import Navbar from './components/Navbar.vue'
+  export default {
+    data() {
+      return {
+        showModal: false,
+      }
+    },
+    methods: {
+      showOrHideModal(){
+        this.showModal = false
+      },
+    },
+    
+    components: { Navbar, Modal },
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+<Transition name="modal">
+  <div class="modal" @click="closeModal">
+    <div class="modal-block" @click.stop>
+      <div class="modal-content">
+        <h2>Добавить заметку</h2>
+        <input type="text" placeholder="Tile" v-model="title" />
+        <textarea placeholder="Content" v-model="descr"></textarea>
+        <div class="modal-btns">
+          <a href="#" class="btn cancel" @click.prevent="closeModal">ОТМЕНА</a>
+          <a href="#" class="btn">ДОБАВИТЬ</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</Transition >
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      descr: "",
+    };
+  },
+  methods: {
+    closeModal() {
+        this.$emit("showOrHideModal")
+        this.title = this.descr = ""
+    },
+  },
+};
+</script>
+
+<style>
+</style>
